96df813986ac2069e72cc84b3744a9fa71cb7cfc
Nguyen Thi Tinh <nguyenthitinh@Nguyens-MacBook-Pro.local>
2021-08-12 19:20:32 +0700

    Doi tien tie USD-to-VND

---
+++ changemoney.py
@@ -0,0 +1,5 @@
+# coding utf-8
+import math
+c = float(input("Số Dola cần chuyển là="))
+vnd = c * 23500
+print("Số tiền USD cần đổi là {c} DOLA \n So tien VietNam la {vnd} VND".format(c=c,vnd=vnd))
