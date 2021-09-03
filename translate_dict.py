800b77a38cc2c24110ca6120b16ff3cdc54a2077
HUONGHUA <87414942+HUONGHUA@users.noreply.github.com>
2021-09-03 21:09:34 +0700

    Update translate_dict.py

--- translate_dict.py
+++ translate_dict.py
@@ -16,5 +16,7 @@ def tran_english(eng,vn):
     for word in eng:
         if eng[word] == vn:
             print(eng[word] ,":" ,word)
+        else:
+            print("Sorry ! This word not available")
 input_word = input("Pleae enter word to translate VN: ")
 tran_english(reverse_dic,input_word)
