cd3c4b0abb3f4c42544d70c42893ef48fa33dc3e
HUONGHUA <87414942+HUONGHUA@users.noreply.github.com>
2021-09-03 21:07:34 +0700

    Create translate_dict.py

---
+++ translate_dict.py
@@ -0,0 +1,20 @@
+data_translate = {
+    'one': 'một',
+    'two': 'hai',
+    'three': 'ba',
+    'four': ' bon',
+    'five': 'nam',
+    'six': 'sau',
+    'seven': 'bay',
+    'eight': 'tam',
+    'nine': 'chin',
+    'ten': 'muoi',
+}
+reverse_dic = {data_translate[k] : k for k in data_translate}
+print(reverse_dic)
+def tran_english(eng,vn):
+    for word in eng:
+        if eng[word] == vn:
+            print(eng[word] ,":" ,word)
+input_word = input("Pleae enter word to translate VN: ")
+tran_english(reverse_dic,input_word)
